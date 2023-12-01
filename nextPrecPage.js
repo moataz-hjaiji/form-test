dcb77460494ca7a03ee475c8d57b23509da84bc4
moataz hjaiji <moatazhjaiji@takiacademyteam.com>
2023-12-01 15:39:22 +0100

    update change

--- nextPrecPage.js
+++ nextPrecPage.js
@@ -1,4 +1,9 @@
-function createNext
+function createNextPreButton(){
+  const questionResponseContainer = Array.from(document.querySelectorAll('.question-response'));
+  questionResponseContainer.map((qr,index)=>{
+    
+  })
+}
 
 const nextPage(){
 
