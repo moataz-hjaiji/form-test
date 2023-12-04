b653596d5c32a9978762fec199ea542a7f91a956
moataz hjaiji <moatazhjaiji@takiacademyteam.com>
2023-12-04 09:17:50 +0100

    feat: next previous form question

--- displayQuestion.js
+++ displayQuestion.js
@@ -1,13 +1,71 @@
+// function displayForm() {
+//   const questionResponseContainers = document.querySelectorAll('.question-response');
+
+//   questionResponseContainers.forEach((qr) => {
+//     qr.addEventListener('click', (event) => {
+//       // Check if the click target is an input or textarea
+//       const isInputField = event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA';
+
+//       // Toggle the class only if not clicking on an input or textarea
+//       if (!isInputField) {
+//         questionResponseContainers.forEach((otherQr) => {
+//           if (otherQr !== qr) {
+//             otherQr.classList.remove('show-question-form');
+//           }
+//         });
+//         qr.classList.toggle('show-question-form');
+//       }
+//     });
+//   });
+// }
+
+// displayForm();
+
+// function displayForm() {
+//   console.log('hey');
+//   const questionResponseContainer = Array.from(document.querySelectorAll('.question-response'));
+
+//   questionResponseContainer.map((qr) => {
+//     qr.addEventListener('click', (event) => {
+//       if (!event.target.closest('form')) {
+//         qr.classList.toggle('show-question-form');
+//       }
+//     });
+//   });
+// }
+
+// displayForm();
+// function displayForm() {
+//   const questionResponseContainers = document.querySelectorAll('.question-response');
+
+//   questionResponseContainers.forEach((qr) => {
+//     qr.addEventListener('click', () => {
+//       questionResponseContainers.forEach((otherQr) => {
+//         if (otherQr !== qr) {
+//           otherQr.classList.remove('show-question-form');
+//         }
+//       });
+//       qr.classList.toggle('show-question-form');
+//     });
+//   });
+// }
+
+// displayForm();
 function displayForm() {
   const questionResponseContainers = document.querySelectorAll('.question-response');
-
+ 
   questionResponseContainers.forEach((qr) => {
-    qr.addEventListener('click', (event) => {
-      if (!event.target.closest('.response')) {
-        qr.classList.toggle('show-question-form');
-      }
+    const questionDiv = qr.querySelector('.question');
+    questionDiv.addEventListener('click', () => {
+      questionResponseContainers.forEach((otherQr) => {
+        if (otherQr !== qr) {
+          otherQr.classList.remove('show-question-form');
+        }
+      });
+      qr.classList.toggle('show-question-form');
     });
   });
-}
-
-displayForm();
+ }
+ 
+ displayForm();
+ 
--- nextPrecPage.js
+++ nextPrecPage.js
@@ -1,11 +1,53 @@
-function createNextPreButton(){
+
+// type : next - prec
+const createButton= (type,content)=> {
+  const button = document.createElement('button');
+  button.classList.add(`${type}-button`);
+  button.type = "button";
+  button.textContent = content;
+  return button;
+}
+
+
+function createNextPreviousButton(){
   const questionResponseContainer = Array.from(document.querySelectorAll('.question-response'));
-  questionResponseContainer.map((qr,index)=>{
-    
+  questionResponseContainer.forEach((qr,index)=>{
+    let response = qr.querySelector('.response');
+    response.innerHTML += `<span class="button-next-prec"></span>`;
+    let containerNextPrec = response.querySelector('.button-next-prec')
+    if(index===0){
+      containerNextPrec.appendChild(createButton('next','Suivant'));
+    }else if (index===questionResponseContainer.length-1){
+      containerNextPrec.appendChild(createButton('prec','Précédent'));
+    }else{
+      // response.innerHTML += `<span class="button-next-prec"></span>`
+      containerNextPrec.appendChild(createButton('prec','Précédent'));
+      containerNextPrec.appendChild(createButton('next','Suivant'));
+    }
   })
 }
 
-const nextPage(){
-
-
+function nextForm(){
+  const nextButtons = Array.from(document.querySelectorAll('.next-button'));
+  const questionResponseConainer = Array.from(document.querySelectorAll('.question-response'))
+  for(let i =0;i<nextButtons.length; i ++){
+    nextButtons[i].addEventListener('click',()=>{
+      questionResponseConainer[i].classList.remove('show-question-form');
+      questionResponseConainer[i+1].classList.add('show-question-form');
+    })
+  }
 }
+function precForm(){
+  const precButtons = Array.from(document.querySelectorAll('.prec-button'));
+  const questionResponseConainer = Array.from(document.querySelectorAll('.question-response'))
+  for(let i =precButtons.length-1;i>=0; i--){
+    console.log(i)
+    precButtons[i].addEventListener('click',()=>{
+      questionResponseConainer[i+1].classList.remove('show-question-form');
+      questionResponseConainer[i].classList.add('show-question-form');
+    })
+  }
+}
+createNextPreviousButton();
+nextForm();
+precForm();
